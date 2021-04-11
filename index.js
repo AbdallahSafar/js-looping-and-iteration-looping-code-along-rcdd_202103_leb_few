b8008a27a655b56bda83cef6c4ac4383d4eb1179
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-11 12:04:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names,ev)
+{
+  let array;
+  for(let i=0;i<names.length;i++)
+  {
+    names[i] = `Thank you, ${names[i]}, for the wonderful ${ev} gift!``;
+  }
+  return names;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
